16d322173ed383dcbf4b4f2439b90939f7da9134
Namratha S N <73111768+namrathasn@users.noreply.github.com>
2022-12-15 22:39:54 +0530

    Add files via upload

---
+++ QoutientRemainder.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class QoutientRemainder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The Number");
+        int num = sc.nextInt();
+        System.out.println("Enter the Divisor");
+        int d = sc.nextInt();
+        //generating quotient
+        int q = num / d;
+        //generating remainder
+        int r = num % d;
+        System.out.println("Quotient: " + q);
+        System.out.println("Remainder: " + r);
+    }
+
+}
